36d08202b7d905e8cc62514bed50480376d64726
SeongBae-applepie <103756855+SeongBae-applepie@users.noreply.github.com>
2023-08-12 13:06:15 +0900

    Add files via upload

--- 성배 nodejs 파일/app.js
+++ 성배 nodejs 파일/app.js
@@ -1,14 +1,17 @@
-var app = require("express")();ㅈ
-var http = require("http").createServer(app);
+const express = require("express");
+const mysql = require("mysql2");
+const dbconfig = require("./config/db_config.js");
+const conn = mysql.createConnection(dbconfig);
+
+var http = require("http").createServer(express);
 var io = require("socket.io")(http);
 
 var port = 3000;
 
 io.on("connection", function (socket) {
-
   //클라이언트 연결되면
   console.log(socket.id, "Connected"); //연결됨을 콘솔에 출력
-  
+
   var id_message = {
     id: `${socket.id} 나의 아이디`,
   };
@@ -18,35 +21,38 @@ io.on("connection", function (socket) {
 
   //클라이언트에게 msg가 emit되면 실행
   socket.on("msg", function (data) {
+    conn.connect();
+
+    var sql = "select * from env order by id desc limit 1";
+
+    conn.query(sql, function (err, rows, fields) {
+      console.log(rows);
+
+      var message = {
+        hum: rows[0].hum,
+        tem: rows[0].temp,
+        w_tem: rows[0].wtemp,
+        lig: rows[0].light,
+      };
+
+      socket.emit("msg_to_client", message);
+
+      if (err) {
+        console.error("error connecting: " + err.stack);
+      }
+    });
 
     console.log("-------------------");
     console.log(socket.id, data);
     console.log("id: ", socket.id);
     console.log("-------------------");
-
-    //클라이언트에게 전달한 메세지 만들기
-    var message = {
-      //   hum: `hum : ${data.id}`,
-      //   tem: `tem : ${data.id}`,
-      //   w_tem: `w_tem : ${data.id}`,
-      //   lig: `lig: ${data.id}`,
-      hum: `hum : 30`,
-      tem: `tem : 25`,
-      w_tem: `w_tem : 23`,
-      lig: `lig: 4`,
-    };
-
-    //클라이언트에게 메세지 전송 
-    socket.emit("msg_to_client", message); //클라이언트로 메시지 전송
   });
-
 
   socket.on("disconnect", function () {
     //클라이언트 연결 끊어지면 자동 실행
+    conn.end(); // 연결 해제
     console.log("disconnected");
   });
-
-
 });
 
 http.listen(port, () => {
